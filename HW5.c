0dce33b0d57e1b0fe4d0f3881d84de56438d457a
liavr6 <56167356+liavr6@users.noreply.github.com>
2020-06-02 17:21:04 +0300

    Update HW5.c

--- HW5.c
+++ HW5.c
@@ -1,17 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+# define _CRT_SECURE_NO_WARNINGS
 # define NAME_LENGTH 200
 # define LINE_LENGTH 400
 
-char *readLine(FILE *fPtrRead, FILE *fPtrWrite, char tav);
+char *readLine(FILE *fPtrRead, char tav);
 int rename_func();
 int returnd_from_cos_func();
 int production_func();
 int fatal_func();
 int fire_func();
-HW_component* add_last(char *name, int copy);
-HW_component* create_node(char *name, int copy);
-//int createlist(char name, int copy);
-
+char *split(char *buf);
+HW_component *createinlist(char *name, int copynum, int grade, HW_component *newlist);
+HW_component *add_cmp(HW_component *head, HW_component *cmp);
+HW_component* new_cmp(char *name, int copynum, int grade);
+void free_list(HW_component *head);
 // define struct
 typedef struct hw_component
 {
@@ -20,26 +25,19 @@ typedef struct hw_component
 	struct hw_component *next;
 }HW_component;
 
-typedef struct node
-{
-	int data;
-	struct node *next;
-} node_t;
-
-
-char *readLine(FILE *fPtrRead, FILE *fPtrWrite, char tav)
+char *readLine(FILE *fPtrRead, char tav)
 {
 	char *temp = NULL;
 	if (NULL == (temp = malloc(LINE_LENGTH * sizeof(char) + 1)))
-		return 1;
+		exit(1);
 	int len = 0;
-	while (tav != '\n' && !feof(fPtrRead) && len<LINE_LENGTH)
+	while (tav != '\n' && !feof(fPtrRead) && len < LINE_LENGTH)
 	{
 		temp[len] += tav;
-		//fprintf(fPtrWrite, "%c", tav);
 		fscanf(fPtrRead, "%c", &tav);
 		len++;
 	}
+	return temp;
 }
 
 int rename_func()
@@ -75,99 +73,116 @@ int main(int argc, char* argv[])
 	if (fPtrRead == NULL || fPtrWrite == NULL) { printf("File did not open. Exit..\n"); }
 
 	fscanf(fPtrRead, "%c", &tav);
+	char *nextorder = NULL;
 	nextline = readLine(fPtrRead, fPtrWrite, tav);
-	while (!feof(fPtrRead) || strcmp(nextline, laststring)) {
-		switch (3)//substring nextline
-		{
-		case 1:
-			add_last(nextline,nextline);//substring here
-			break;
-		case 2:
-			
-			break;
-		case 3:
-			
-			break;
-		case 4:
-			
-			break;
-		case 5:
-			
-			break;
-		case 6:
-			
+	nextorder = split(readLine)[0];
+	while (!feof(fPtrRead))
+	{
+		nextorder = split(readLine)[0];
 
+		if (strcmp(nextorder, "Initialize") == 0)
+		{
+			continue;
+			// do something
+		}
+		else if (strcmp(nextorder, "Finalize") == 0)
+		{
 			break;
-		default:
+			// do something else
+		}
+		else if (strcmp(nextorder, "Rename") == 0)
+		{
+			// do something else
+		}
+		else if (strcmp(nextorder, "Returned_from_customer") == 0)
+		{
+			// do something else
+		}
+		else if (strcmp(nextorder, "Production") == 0)
+		{
+			// do something else
+		}
+		else if (strcmp(nextorder, "Fatal_malfunction") == 0)
+		{
+			// do something else
+		}
+		else if (strcmp(nextorder, "Fire") == 0)
+		{
+			// do something else
+		}
+		/* more else if clauses */
+		else /* default: */
+		{
 			break;
 		}
-		//readLine(fPtrRead, fPtrWrite, tav);
-		//fprintf(fPtrWrite, "\n\n");
-		//fscanf(fPtrRead, "%c", &tav);
+
 		nextline = readLine(fPtrRead, fPtrWrite, tav);
 	}
 	fclose(fPtrRead);
 	fclose(fPtrWrite);
 }
 
-//int createlist(char *name,int copy)
-//{
-//	int value = 0;
-//	HW_component *list = NULL, *new_node = NULL;
-//
-//	//printf("enter values (negative to stop)\n");
-//	//scanf("%d", &value);
-//	while (value >= 0) {
-//		new_node = (HW_component*)malloc(sizeof(HW_component));
-//		if (new_node == NULL)
-//		{ 
-//			printf("Fatal error: memory allocation failed!\n");           
-//		return 1; 
-//		}   
-//		
-//		strcpy(new_node->name,name);
-//		new_node->copies = copy; 
-//		new_node->next=list;
-//		list = new_node;
-//
-//		//scanf("%d", &value);
-//	}
-//
-//	/* print the number of input values */
-//
-//	/* free list */
-//	return 0;
-//}
-HW_component* create_node(char *name, int copy)
+/* Free a list of comps */
+void free_list(HW_component *head)
 {
-	int value = 0;
-	HW_component *list = NULL, *new_node = NULL;
-	new_node = (HW_component*)malloc(sizeof(HW_component));
-
-	node_t *new_node = (node_t*)malloc(sizeof(node_t));
-	if (new_node != NULL) {
-		strcpy(new_node->name, name);
-		new_node->copies = copy;
-		new_node->next = list;
+	if (head != NULL)
+	{
+		free_list(head->next);
+		free(head);
 	}
-	return new_node;
 }
-HW_component* add_last(char *name, int copy)
+
+/* Allocate a new comps */
+HW_component* new_cmp(char *name, int copynum, int grade)
+{
+	HW_component *std = NULL;
+
+	std = (HW_component*)malloc(sizeof(HW_component));
+	if (std == NULL)
+	{
+		printf("Memory allocation error!\n");
+		return NULL;
+	}
+
+	strcpy(std->name, name);
+	strcpy(std->copies, copynum);
+	std->next = NULL;
+
+	return std;
+}
+HW_component *add_cmp(HW_component *head, HW_component *cmp)
 {
 	HW_component *tail;
-	HW_component *list = NULL, *new_node = NULL;
-
-	HW_component *new_node = create_node(name,copy);
-	if (new_node == NULL)
+	if (cmp == NULL)
 		return NULL;
 
-	if (list == NULL)
-		return new_node;
+	if (head == NULL)
+		return cmp;
 
-	tail = list;
+	tail = head;
 	while (tail->next != NULL)
 		tail = tail->next;
-	tail->next = new_node;
+	tail->next = cmp;
 
-	return list;
+	return head;
+}
+
+HW_component *createinlist(char *name, int copynum, int grade, HW_component *newlist)
+{
+	return add_cmp(newlist,new_cmp(name, copynum, grade));
+}
+
+char *split(char *buf)
+{
+	int i = 0;
+	char *p = strtok(buf, "$$$");
+	char *array[LINE_LENGTH];
+
+	while (p != NULL && *p !='\n')
+	{
+		array[i++] = p;
+		p = strtok(NULL, "$$$");
+	}
+
+	return array;
 }
