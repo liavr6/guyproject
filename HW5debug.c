da3ec798007da816a6346787386066e8e35a6292
liavr6 <56167356+liavr6@users.noreply.github.com>
2020-06-10 14:13:01 +0300

    Update HW5debug.c

--- HW5debug.c
+++ HW5debug.c
@@ -19,16 +19,12 @@ int fatal_func(HW_component *list, char name[NAME_LENGTH], int mal_num);
 int fire_func(HW_component *list, char name[NAME_LENGTH], int mal_num);
 char *split(char *buf, int iter);
 HW_component *createinlist(char *name, int copynum, HW_component *newlist);
-//HW_component *add_cmp(HW_component *head, HW_component *cmp);
 HW_component *new_cmp(char *name, int copynum);
 void free_list(HW_component *head);
 HW_component* find(HW_component *head, char *val);
 HW_component* add_and_sort_alpha(HW_component *head, HW_component *new_node);
 void printlisttofile(HW_component *list, FILE *fPtrWrite);
-void trimTrailing(char * str);
-void trimLeading(char * str);
 int length(const HW_component *head);
-HW_component *delete(HW_component *head, char *val);
 
 int main(int argc, char* argv[])
 {
@@ -36,122 +32,109 @@ int main(int argc, char* argv[])
 	HW_component *compnodelist = NULL;
 	char	tav = '0';
 	char *nextline = NULL;
+	int flag = 0;
 	//char orderfile = argv[2], compfile = argv[1], fPtr = argv[3];
 	char orderfile[100] = "C:\\Users\\Liav\\Desktop\\txtfiles\\actions.txt";
 	char compfile[100] = "C:\\Users\\Liav\\Desktop\\txtfiles\\hw_components.txt";
 	char fPtr[100] = "C:\\Users\\Liav\\Desktop\\txtfiles\\updated222_components.txt";
+	if (sizeof(argv) != 4) {
+		flag = 1;
+		printf("Error: invalid number of arguments (<%d> instead of 3)\n", sizeof(argv));
+	}
+
 	orderfileread = fopen(orderfile, "r");
+	if (orderfileread == NULL) { flag = 1; printf("Error: opening %s failed\n", orderfileread); }
 	compfileread = fopen(compfile, "r");
+	if (compfileread == NULL) { flag = 1; printf("Error: opening %s failed\n", compfileread); }
 	fPtrWrite = fopen(fPtr, "w");
-	if (orderfileread == NULL || fPtrWrite == NULL) { printf("File did not open. Exit..\n"); }
+	if (fPtrWrite == NULL) { flag = 1; printf("Error: opening %s failed\n", fPtrWrite); }
 
-	char *nextorder = NULL;
-	//nextline = readLine(orderfileread, tav);
-	//nextorder = split(nextline, 1);
-	char line[LINE_LENGTH], name[NAME_LENGTH];
-	while (fgets(line, LINE_LENGTH, orderfileread) != NULL)
+	if (flag != 1)
 	{
-		nextorder = split(line, 0);
-
-
-		HW_component* last = NULL;
-
-		if (strstr(nextorder, "Initialize") != NULL)
+		char *nextorder = NULL;
+		char line[LINE_LENGTH], name[NAME_LENGTH];
+		while (fgets(line, LINE_LENGTH, orderfileread) != NULL)
 		{
-	
-			int nextcopies = 0;
-			
-			char linecomp[LINE_LENGTH], namecomp[NAME_LENGTH];
-			while (fgets(linecomp, LINE_LENGTH, compfileread) != NULL)
+			nextorder = split(line, 0);
+			HW_component* last = NULL;
+
+			if (strstr(nextorder, "Initialize") != NULL)
 			{
-				strcpy(namecomp,split(linecomp, 0));////////////////////////check if 0 and 1
-				nextcopies = atoi(split(linecomp, 1));
-				//linecomp = readLine(compfileread, tav);
-				compnodelist = createinlist(namecomp, nextcopies, compnodelist);
+
+				int nextcopies = 0;
+				char linecomp[LINE_LENGTH], namecomp[NAME_LENGTH];
+				while (fgets(linecomp, LINE_LENGTH, compfileread) != NULL)
+				{
+					//_CrtDumpMemoryLeaks();
+
+					strcpy(namecomp, split(linecomp, 0));////////////////////////check if 0 and 1
+					nextcopies = atoi(split(linecomp, 1));
+					compnodelist = createinlist(namecomp, nextcopies, compnodelist);
+					//free(tempdata);
+				}
+				last = compnodelist;
+
 			}
-			fclose(compfileread);
-			last = compnodelist;
-			//FILE *fPtrWrite = fopen("C:\\Users\\Liav\\Desktop\\txtfiles\\rename_components.txt", "w");
-			//printlisttofile(compnodelist, fPtrWrite);
-			//continue;///////////////break?/////////////////////
-			// do something
-		}
-		else if (strstr(nextorder, "Finalize") != NULL)
-		{
-			printlisttofile(compnodelist, fPtrWrite);
-			free_list(compnodelist);
-			break;
-		}
-		else if (strcmp(nextorder, "Rename") == 0)
-		{
-			char oldname[NAME_LENGTH];
-			strcpy(oldname, split(line, 1));
-			char newname[NAME_LENGTH];/////////check this
-			strcpy(newname, split(line, 2));
-			//FILE *fPtrWrite = fopen("C:\\Users\\Liav\\Desktop\\txtfiles\\rename1111_components.txt", "w");
-			//printlisttofile(compnodelist, fPtrWrite);
-			rename_func(compnodelist, oldname, newname);
-			//fPtrWrite = fopen("C:\\Users\\Liav\\Desktop\\txtfiles\\rename2_components.txt", "w");
-			//printlisttofile(compnodelist, fPtrWrite);
-			// do something else
-		}
-		else if (strcmp(nextorder, "Returned_from_customer") == 0)
-		{
-			char name[NAME_LENGTH];
-			strcpy(name, split(line, 1));
-			int copiestoreturn = atoi(split(line, 2));
-			//FILE *fPtrWrite = fopen("C:\\Users\\Liav\\Desktop\\txtfiles\\returnd23123_components.txt", "w");
-			//printlisttofile(compnodelist, fPtrWrite);
-			returnd_from_cos_func(compnodelist, name, copiestoreturn);
-			// do something else
-			//fPtrWrite = fopen("C:\\Users\\Liav\\Desktop\\txtfiles\\returnd_components.txt", "w");
-			//printlisttofile(compnodelist, fPtrWrite);
-		}
-		else if (strcmp(nextorder, "Production") == 0)
-		{
-			char name[NAME_LENGTH];
-			strcpy(name, split(line, 1));
-			int copiestoreturn = atoi(split(line, 2));
-			production_func(compnodelist, name, copiestoreturn);
-			//FILE *fPtrWrite = fopen("C:\\Users\\Liav\\Desktop\\txtfiles\\production_components.txt", "w");
-			//printlisttofile(compnodelist, fPtrWrite);
-			// do something else
-		}
-		else if (strcmp(nextorder, "Fatal_malfunction") == 0)
-		{
-			char name[NAME_LENGTH];
-			strcpy(name, split(line, 1));
-			int mal_num = atoi(split(line, 2));
-			fatal_func(compnodelist, name, mal_num);
-			//FILE *fPtrWrite = fopen("C:\\Users\\Liav\\Desktop\\txtfiles\\fatal_components.txt", "w");
-			//printlisttofile(compnodelist, fPtrWrite);
-		}
-		else if (strcmp(nextorder, "Fire") == 0)
-		{
-			char name[NAME_LENGTH];
-			strcpy(name, split(line, 1));
-			int mal_num = atoi(split(line, 2));
-			fire_func(compnodelist, name, mal_num);
-			//FILE *fPtrWrite = fopen("C:\\Users\\Liav\\Desktop\\txtfiles\\fire_components.txt", "w");
-			//printlisttofile(compnodelist, fPtrWrite);
-		}
-		/* more else if clauses */
-		else /* default: */
-		{
-			break;
-		}
+			else if (strstr(nextorder, "Finalize") != NULL)
+			{
+				printlisttofile(compnodelist, fPtrWrite);
+				free_list(compnodelist);
+				break;
+			}
+			else if (strstr(nextorder, "Rename") != NULL)
+			{
+				char oldname[NAME_LENGTH];
+				strcpy(oldname, split(line, 1));
+				char newname[NAME_LENGTH];/////////check this
+				strcpy(newname, split(line, 2));
 
-		//nextline = readLine(orderfileread, fPtrWrite, tav);
-		//nextline = readLine(orderfileread, tav);
-		//nextorder = split(nextline, 0);
+				rename_func(compnodelist, oldname, newname);
 
+			}
+			else if (strstr(nextorder, "Returned_from_customer") != NULL)
+			{
+				char name[NAME_LENGTH];
+				strcpy(name, split(line, 1));
+				int copiestoreturn = atoi(split(line, 2));
+				returnd_from_cos_func(compnodelist, name, copiestoreturn);
 
+			}
+			else if (strstr(nextorder, "Production") != NULL)
+			{
+				char name[NAME_LENGTH];
+				strcpy(name, split(line, 1));
+				int copiestoreturn = atoi(split(line, 2));
+				production_func(compnodelist, name, copiestoreturn);
 
-		_CrtDumpMemoryLeaks();
+			}
+			else if (strstr(nextorder, "Fatal_malfunction") != NULL)
+			{
+				char name[NAME_LENGTH];
+				strcpy(name, split(line, 1));
+				int mal_num = atoi(split(line, 2));
+				fatal_func(compnodelist, name, mal_num);
+
+			}
+			else if (strstr(nextorder, "Fire") != NULL)
+			{
+				char name[NAME_LENGTH];
+				strcpy(name, split(line, 1));
+				int mal_num = atoi(split(line, 2));
+				fire_func(compnodelist, name, mal_num);
+
+			}
+			/* more else if clauses */
+			else /* default: */
+			{
+				break;
+			}
+
+		}
+		fclose(orderfileread);
+		fclose(fPtrWrite);
+		fclose(compfileread);
 
 	}
-	fclose(orderfileread);
-	fclose(fPtrWrite);
 }
 
 HW_component *find(HW_component *head, char *val)
@@ -200,12 +183,6 @@ int returnd_from_cos_func(HW_component *list, char name[NAME_LENGTH], int copies
 		createinlist(name, copiestoreturn, list);
 	}
 
-	//fPtrWrite = fopen("C:\\Users\\Liav\\Desktop\\txtfiles\\renam123456789_components.txt", "w");
-	//printlisttofile(list, fPtrWrite);
-	//fclose(fPtrWrite);
-	//sort(list);
-	//printlisttofile(list, fPtrWrite);/////////////////////////////////////////////////////////////////
-
 	return 0;
 }
 int production_func(HW_component *list, char name[NAME_LENGTH], int copies)
@@ -232,22 +209,6 @@ int fire_func(HW_component *list, char name[NAME_LENGTH], int mal_num)
 }
 
 
-//char *readLine(FILE *fPtrRead, char tav)
-//{
-//	char *temp = NULL;
-//	if (NULL == (temp = malloc(LINE_LENGTH * sizeof(char) + 1)))
-//		exit(1);
-//	int len = 0;
-//	fscanf(fPtrRead, "%c", &tav);
-//	while (tav != '\n' && !feof(fPtrRead) && len < LINE_LENGTH)
-//	{
-//		temp[len] = tav;
-//		fscanf(fPtrRead, "%c", &tav);
-//		len++;
-//	}
-//	return temp;
-//}
-
 int length(const HW_component *head) {
 	int count = 0;
 	HW_component *iter;
@@ -262,7 +223,7 @@ HW_component* new_cmp(char *name, int copynum)
 	std = (HW_component*)malloc(sizeof(HW_component));
 	if (std == NULL)
 	{
-		printf("Memory allocation error!\n");
+		printf("Error: memory allocation failed\n");
 		return NULL;
 	}
 
@@ -278,7 +239,6 @@ void printlisttofile(HW_component *list, FILE *fPtrWrite)
 	HW_component *iter=list;
 	while (iter != NULL)
 	{
-		//fprintf(fPtrWrite, "%s", iter->name);
 		fprintf(fPtrWrite, "%s $$$ %d\n", iter->name, iter->copies);
 		iter = iter->next;
 	}
@@ -286,7 +246,6 @@ void printlisttofile(HW_component *list, FILE *fPtrWrite)
 HW_component* add_and_sort_alpha(HW_component *head, HW_component *new_node)
 {
 	HW_component* iter, *prev = NULL;
-	//HW_component* new_node = new_cmp(name,copynum);
 	/* incomplete, must check for failure */
 	if (head == NULL)
 		return new_node;
@@ -318,31 +277,10 @@ void free_list(HW_component *head)
 		head = head->next;
 		free(temp);
 	}
+	_CrtDumpMemoryLeaks();
+
 }
 
-
-////HW_component *add_cmp(HW_component *head, HW_component *cmp)
-////{
-////	HW_component *tail;
-////	if (cmp == NULL)
-////		return NULL;
-////
-////	if (head == NULL)
-////	{
-////		head = cmp;
-////		return head;
-////	}
-////
-////
-////	tail = head;
-////	while (tail != NULL) 
-////	{
-////		tail = tail->next;
-////	}
-////	tail->next = cmp;
-////
-////	return head;
-////}
 
 HW_component *createinlist(char *name, int copynum, HW_component *newlist)
 {
@@ -350,118 +288,29 @@ HW_component *createinlist(char *name, int copynum, HW_component *newlist)
 	HW_component *toreturn = add_and_sort_alpha(newlist, new_cmp(name,copynum));
 	return toreturn;
 }
-
 char *split(char *buf, int iter)
 {
-	int i = 0;
-	int dollar = 0;
-	if (strchr(buf, '$')) { dollar = 1; }
-
-	char *temp = NULL;
-	if (NULL == (temp = malloc(LINE_LENGTH * sizeof(char) + 1)))
-		exit(1);
-
-	strcpy(temp, buf);
-
-	char *p = strtok(temp, "$$$");
-
-	if (dollar) {
-		while (p != NULL && *p != '\n' && i < iter)
+	int i, partlen;
+	char *endbit, res[NAME_LENGTH];
+	for (i = 0; i < iter; i++)
+	{
+		buf = strstr(buf, " $$$ ");
+		if (buf == NULL)
 		{
-			//array[i++] = p;
-			p = strtok(NULL, "$$$");
-			i++;
+			return NULL;
 		}
-		if (p != NULL)
-		{
-
-			trimLeading(p);
-			trimTrailing(p);
-		}
+		buf += strlen(" $$$ ");
 	}
-
+	endbit = strstr(buf, " $$$ ");
+	if (endbit != NULL)
+		partlen = strlen(buf) - strlen(endbit);
 	else
-		p = strtok(p, "\n");
+		partlen = strlen(buf);
 
-	//strcpy(buf, temp);
-	//free(temp);
-	return p;
-}
-/**
- * Remove trailing white space characters from string
- */
-void trimTrailing(char * str)
-{
-	int index, i;
+	strncpy(res, buf, partlen);
+	if (res[partlen - 1] == '\n')
+		partlen--;
+	res[partlen] = '\0';
 
-	/* Set default index */
-	index = -1;
-
-	/* Find last index of non-white space character */
-	i = 0;
-	while (str[i] != '\0')
-	{
-		if (str[i] != ' ' && str[i] != '\t' && str[i] != '\n')
-		{
-			index = i;
-		}
-
-		i++;
-	}
-
-	/* Mark next character to last non-white space character as NULL */
-	str[index + 1] = '\0';
-}
-void trimLeading(char * str)
-{
-	int index, i, j;
-
-	index = 0;
-
-	/* Find last index of whitespace character */
-	while (str[index] == ' ' || str[index] == '\t' || str[index] == '\n' || str[index] == NULL)
-	{
-		index++;
-	}
-
-
-	if (index != 0)
-	{
-		/* Shit all trailing characters to its left */
-		i = 0;
-		while (str[i + index] != '\0')
-		{
-			str[i] = str[i + index];
-			i++;
-		}
-		str[i] = '\0'; // Make sure that string is NULL terminated
-	}
-}
-
-HW_component *delete(HW_component *head, char *val)
-{
-	HW_component *iter = head, *prev = NULL;
-
-	if (head == NULL)
-		return head;
-
-	if (head->name == val)
-	{
-		iter = head->next;
-		//free(head);
-		return iter;
-	}
-	prev = iter; iter = iter->next;
-	while (iter != NULL)
-	{
-		if (iter->name == val)
-		{
-			prev->next = iter->next;
-			//free(iter);
-			break;
-		}
-		prev = iter;
-		iter = iter->next;
-	}
-	return head;
+	return res;
 }
